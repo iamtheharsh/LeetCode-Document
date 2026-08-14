8ff5c8cd88e9c5e289f2e08a85d39b1db3d03cd5
Harsh Patel <161930162+iamtheharsh@users.noreply.github.com>
2026-08-14 12:02:12 +0530

    Time: 311 ms (67.03%), Space: 179.2 MB (76.45%) - LeetHub

---
+++ 3999-minimum-number-of-string-groups-through-transformations/3999-minimum-number-of-string-groups-through-transformations.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    string minCyclicRotation(const string& s) {
+        int n = s.size();
+
+        if (n == 0) return "";
+
+        string ss = s + s;
+
+        int i = 0, j = 1, k = 0;
+
+        while (i < n && j < n && k < n) {
+            if (ss[i + k] == ss[j + k]) {
+                k++;
+                continue;
+            }
+
+            if (ss[i + k] > ss[j + k]) {
+                i = i + k + 1;
+                if (i <= j)
+                    i = j + 1;
+            } else {
+                j = j + k + 1;
+                if (j <= i)
+                    j = i + 1;
+            }
+
+            k = 0;
+        }
+
+        int start = min(i, j);
+
+        return ss.substr(start, n);
+    }
+
+    int minimumGroups(vector<string>& words) {
+        int n = words.size();
+        set<string> st;
+        for(auto& w:words){
+            int m = w.size();
+            string odd , even;
+            for(int i=0;i<m;i++){
+                if(i%2) odd += w[i];
+                else even += w[i];
+            }
+            odd = minCyclicRotation(odd);
+            even = minCyclicRotation(even);
+            st.insert(odd+even);
+        }
+        return st.size();
+    }
+};
